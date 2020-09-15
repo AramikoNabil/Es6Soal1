6469ddf1cf18e69f974a91420c8ca61900c13cd7
sleepy81k <nabilnico@gmail.com>
2020-09-15 21:54:28 +0700

    first

---
+++ Soal1.js
@@ -0,0 +1,11 @@
+/// Merubah function menjadi Arrow Function
+
+pondokProgrammer = function(){
+    return "Pondok Programmer";
+}
+
+//Merubah
+
+const pondokProgrammer = () => {
+    return "Pondok Programmer";
+}
